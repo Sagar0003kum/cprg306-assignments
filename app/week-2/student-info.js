d9d4741c5c66d565e5b731951cc7a522c9989aac
sagar <sagarkumbhar326@gmail.com>
2025-09-18 10:26:40 -0600

    Week 2 assignment complete

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Sagar Kumbhar</p>
+      <p>
+        <Link href="https://github.com/Sagar0003kum/cprg306-assignments" target="_blank">
+          My GitHub Repository
+        </Link>
+      </p>
+    </div>
+  );
+}
